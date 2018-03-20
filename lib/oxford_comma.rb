9e5f5cd3459cb2eaccee3e544a7526f00960f5ca
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 19:23:55 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     array.join(" and ")
   elsif array.size >= 3
     # code here
-    begin_string = array[0..-2].join(", ") 
+    begin_string = array.join(", ") 
     end_string = array[-1].join(", and ")
     begin_string << end_string
   # elsif array.size > 3
