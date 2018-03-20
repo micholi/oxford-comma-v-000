c7e689bde4317e60e367c52597b6636b1bd4db62
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 19:00:08 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,5 +3,9 @@ def oxford_comma(array)
     array.join
   elsif array.size == 2
     array.join(" and ")
+  elsif array.size == 3
+    # code here
+  elsif array.size > 3
+    # code here
   end
 end
