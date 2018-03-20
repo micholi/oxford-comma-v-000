c31923b23b777e17d5f8126c40a80840d5481f51
Michelle Olivieri <michelleo72@gmail.com>
2018-03-20 19:35:36 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,8 @@ def oxford_comma(array)
   elsif array.size == 2
     array.join(" and ")
   elsif array.size >= 3
-    # code here
     last = array[-1]
     begin_string = array[0..-2].join(", ")
-    begin_string << "and #{last}."
+    begin_string << ", and #{last}."
   end
 end
